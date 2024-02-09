db031d8edfd098835f6446a27ea50de7097c449b
Kamogelo Motlhale <131041491+JabbzTheCoder@users.noreply.github.com>
2024-02-09 20:42:21 +0200

    Update: 2024-02-09 20:42:21

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,50 @@
 #!/usr/bin/python3
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """
+    BaseModel class for other classes to inherit common attributes and methods.
+
+    Attributes:
+        id (str): Unique identifier generated using uuid.uuid4().
+        created_at (datetime): Date and time when the instance is created.
+        updated_at (datetime): Date and time when the instance is last updated.
+    """
+
+    def __init__(self):
+        """
+        Initializes a new instance of the BaseModel class.
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        Returns a string representation of the BaseModel instance.
+
+        Returns:
+            str: String representation of the BaseModel instance.
+        """
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the updated_at attribute with the current datetime.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary representation of the BaseModel instance.
+
+        Returns:
+            dict: Dictionary representation of the BaseModel instance.
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = type(self).__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
